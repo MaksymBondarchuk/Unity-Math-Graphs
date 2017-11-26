b44c0eb60a28c46e6b576a2a287ade5d2d1e91e3
ShcherbakovaGalina <galena301094@gmail.com>
2017-11-26 20:31:22 +0200

    With middle move

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -9,6 +9,14 @@ public class Main : MonoBehaviour
     //public Camera MainCamera2 { get; set; }
     public GameObject Sphere { get; set; }
 
+    private Vector3 RightClick { get; set; }
+
+    private bool IsRightClicked { get; set; }
+
+    private Vector3 MiddleClick { get; set; }
+
+    private bool IsMiddleClicked { get; set; }
+
     // Use this for initialization
     void Start()
     {
@@ -21,31 +29,50 @@ public class Main : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
             OnMouseDown();
-        //Debug.Log("Pressed left click.");
 
+        #region MoveCamera
         if (Input.GetMouseButtonDown(1))
         {
-            Debug.Log("Pressed right click.");
-            MoveCamera(1);
+            //Debug.Log("Pressed right click.");
+            RightClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+            IsRightClicked = true;
         }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            //Debug.Log("Released right click.");
+            IsRightClicked = false;
+        }
+
+        if (IsRightClicked)
+            MoveRightPressed();
 
         if (Input.GetMouseButtonDown(2))
-            Debug.Log("Pressed middle click.");
+        {
+            //Debug.Log("Pressed middle click.");
+            MiddleClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+            IsMiddleClicked = true;
+        }
+
+        if (Input.GetMouseButtonUp(2))
+        {
+            //Debug.Log("Released middle click.");
+            IsMiddleClicked = false;
+        }
+
+        if (IsMiddleClicked)
+            MoveMiddlePressed();
+        #endregion MoveCamera
 
         if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
-        {
             MoveCamera(1);
-        }
         else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
-        {
             MoveCamera(-1);
-        }
-
     }
 
     void OnMouseDown()
     {
-        Debug.Log(string.Format("{0}, {1}, {2}", Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+        //Debug.Log(string.Format("{0}, {1}, {2}", Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
         var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
 
         var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
@@ -53,15 +80,38 @@ public class Main : MonoBehaviour
         var objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
         sphere.transform.position = objPosition;
         //sphere.GetComponent<Renderer>().material.color = Color.red;
-        Debug.Log(string.Format("{0}, {1}, {2}", objPosition.x, objPosition.y, objPosition.z));
+        //Debug.Log(string.Format("{0}, {1}, {2}", objPosition.x, objPosition.y, objPosition.z));
 
         //myNodes.Add(sphere);
     }
 
-    void MoveCamera(int step)
+    private void MoveCamera(int step)
     {
         var pos = transform.position;
         pos.z += step;
         transform.position = pos;
+    }
+
+    private void MoveRightPressed()
+    {
+        const float velocity = (float)0.01;
+        var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        var pos = transform.rotation;
+        Debug.Log(string.Format("Rot {0}, {1}, {2}", pos.x, pos.y, pos.z));
+        pos.x += velocity * (MiddleClick.x - mousePosition.x);
+        //pos.y += velocity * (MiddleClick.y - mousePosition.y);
+        transform.rotation = pos;
+        MiddleClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+    }
+
+    private void MoveMiddlePressed()
+    {
+        const float velocity = (float) 0.1;
+        var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        var pos = transform.position;
+        pos.x += velocity * (MiddleClick.x - mousePosition.x);
+        pos.y += velocity * (MiddleClick.y - mousePosition.y);
+        transform.position = pos;
+        MiddleClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
     }
 }
