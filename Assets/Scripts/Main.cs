cd21653146e1b600cdf41aaac21b5d99e83ae121
GalenA301094 <galena301094@gmail.com>
2017-12-10 21:18:56 +0200

    With edges

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,55 +1,82 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
 
-public class Main : MonoBehaviour
+namespace Assets.Scripts
 {
-    private List<Transform> HitObject { get; set; }
-
-    // ReSharper disable once UnusedMember.Local
-    void Start()
+    // ReSharper disable once UnusedMember.Global
+    public class Main : MonoBehaviour
     {
-        HitObject = new List<Transform>();
-    }
+        private List<Transform> HitObject { get; set; }
+        private Transform FirstNode { get; set; }
 
-    // ReSharper disable once UnusedMember.Local
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-            AddNode();
-
-        foreach (var objectHit in HitObject)
-            objectHit.GetComponent<Renderer>().material.color = Color.gray;
-        HitObject.Clear();
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        // ReSharper disable once UnusedMember.Local
+        void Start()
         {
-            Transform objectHit = hit.transform;
-            objectHit.GetComponent<Renderer>().material.color = Color.blue;
-            HitObject.Add(objectHit);
+            HitObject = new List<Transform>();
         }
-    }
+
+        // ReSharper disable once UnusedMember.Local
+        void Update()
+        {
+            Transform objectHit = null;
+
+            if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.L))
+                AddNode();
+
+            foreach (var objectHited in HitObject)
+                objectHited.GetComponent<Renderer>().material.color = Color.gray;
+            HitObject.Clear();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                objectHit = hit.transform;
+                objectHit.GetComponent<Renderer>().material.color = Color.blue;
+                HitObject.Add(objectHit);
+            }
+
+            if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.L) && objectHit != null)
+            {
+                if (FirstNode == null)
+                    FirstNode = objectHit;
+                else
+                {
+                    OnDrawGizmosSelected(FirstNode, objectHit);
+                    FirstNode = null;
+                }
+            }
+        }
 
 
-    // ReSharper disable once UnusedMember.Local
-    //private void Update()
-    //{
-    //    if (Input.GetMouseButtonDown(0))
-    //        AddNode();
-    //}
+        // ReSharper disable once UnusedMember.Local
+        //private void Update()
+        //{
+        //    if (Input.GetMouseButtonDown(0))
+        //        AddNode();
+        //}
 
-    void AddNode()
-    {
-        //Debug.Log(string.Format("{0}, {1}, {2}", Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
-        var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        private static void AddNode()
+        {
+            //Debug.Log(string.Format("{0}, {1}, {2}", Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+            var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
 
-        var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        mousePosition.z = 10;
-        var objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        sphere.transform.position = objPosition;
-        sphere.GetComponent<Renderer>().material.color = Color.red;
+            var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            mousePosition.z = 10;
+            var objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            sphere.transform.position = objPosition;
+            sphere.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        private static void OnDrawGizmosSelected(Transform node1, Transform node2)
+        {
+            var cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+
+            const float radius = (float) .1;
+            cylinder.transform.localScale = new Vector3(radius, Vector3.Distance(node2.position, node1.position)/2, radius);
+
+
+            cylinder.transform.position = node1.position + (node2.position - node1.position)/2;
+            cylinder.transform.up = node2.position - node1.position;
+        }
     }
 }
--- Assets/Scripts/MouseHandler.cs
+++ Assets/Scripts/MouseHandler.cs
@@ -1,119 +1,120 @@
-﻿using System;
-using UnityEngine;
-using System.Collections;
+﻿using UnityEngine;
 
-public class MouseHandler : MonoBehaviour
+namespace Assets.Scripts
 {
-    //private ArrayList myNodes;
-    public Camera MainCamera { get; set; }
-    //public Camera MainCamera2 { get; set; }
-    public GameObject Sphere { get; set; }
-
-    private Vector3 RightClick { get; set; }
-
-    private bool IsRightClicked { get; set; }
-
-    private Vector3 MiddleClick { get; set; }
-
-    private bool IsMiddleClicked { get; set; }
-
-    // ReSharper disable once UnusedMember.Local
-    private void Start()
+    public class MouseHandler : MonoBehaviour
     {
-        Cursor.visible = true;
-        //myNodes = new ArrayList();
-    }
+        //private ArrayList myNodes;
+        //public Camera MainCamera { get; set; }
+        //public Camera MainCamera2 { get; set; }
+        //public GameObject Sphere { get; set; }
 
-    // ReSharper disable once UnusedMember.Local
-    private void Update()
-    {
-        #region MoveCamera
-        if (Input.GetMouseButtonDown(1))
+        private Vector3 RightClick { get; set; }
+
+        private bool IsRightClicked { get; set; }
+
+        private Vector3 MiddleClick { get; set; }
+
+        private bool IsMiddleClicked { get; set; }
+
+        // ReSharper disable once UnusedMember.Local
+        private void Start()
         {
-            //Debug.Log("Pressed right click.");
+            Cursor.visible = true;
+            //myNodes = new ArrayList();
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        private void Update()
+        {
+            #region MoveCamera
+            if (Input.GetMouseButtonDown(1))
+            {
+                //Debug.Log("Pressed right click.");
+                RightClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+                IsRightClicked = true;
+            }
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                //Debug.Log("Released right click.");
+                IsRightClicked = false;
+            }
+
+            if (IsRightClicked)
+                MoveRightPressed();
+
+            if (Input.GetMouseButtonDown(2))
+            {
+                //Debug.Log("Pressed middle click.");
+                MiddleClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+                IsMiddleClicked = true;
+            }
+
+            if (Input.GetMouseButtonUp(2))
+            {
+                //Debug.Log("Released middle click.");
+                IsMiddleClicked = false;
+            }
+
+            if (IsMiddleClicked)
+                MoveMiddlePressed();
+            #endregion MoveCamera
+
+            if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+                MoveCamera(1);
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+                MoveCamera(-1);
+        }
+
+        private void MoveCamera(int step)
+        {
+            if (step > 0)
+                transform.Translate(Vector3.forward);
+            else
+                transform.Translate(Vector3.back);
+            //var pos = transform.position;
+            //pos.z += step;
+            //transform.position = pos;
+        }
+
+        private void MoveRightPressed()
+        {
+            //const float velocity = (float)0.001;
+
+            var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+            //var rot = transform.rotation;
+            ////Debug.Log(string.Format("Rot {0}, {1}, {2}", pos.x, pos.y, pos.z));
+            //rot.x += velocity * (RightClick.y - mousePosition.y);
+            ////pos.x += (float)0.01;
+            //rot.y -= velocity * (RightClick.x - mousePosition.x);
+            //transform.rotation = rot;
+            //RightClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+            //transform.RotateAround(mousePosition, Vector3.up, Input.GetAxis("Mouse X") * velocity);
+
+            //float tiltAroundZ = Input.GetAxis("Horizontal") * 15;
+            //float tiltAroundX = Input.GetAxis("Vertical") * 15;
+            //Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 15);
+
+            transform.RotateAround(transform.position, new Vector3(1.0f, 0.0f, 0.0f), (RightClick.y - mousePosition.y) * Time.deltaTime * 5);
+            transform.RotateAround(transform.position, new Vector3(0.0f, 1.0f, 0.0f), -(RightClick.x - mousePosition.x) * Time.deltaTime * 5);
+            transform.RotateAround(transform.position, new Vector3(0.0f, 0.0f, 1.0f), (RightClick.z - mousePosition.z) * Time.deltaTime * 5);
             RightClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-            IsRightClicked = true;
         }
 
-        if (Input.GetMouseButtonUp(1))
+        private void MoveMiddlePressed()
         {
-            //Debug.Log("Released right click.");
-            IsRightClicked = false;
-        }
+            //const float speed = (float)0.1;
+            var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+            //var pos = transform.position;
+            //pos.x += speed * (MiddleClick.x - mousePosition.x);
+            //pos.y += speed * (MiddleClick.y - mousePosition.y);
+            //transform.position = pos;
 
-        if (IsRightClicked)
-            MoveRightPressed();
-
-        if (Input.GetMouseButtonDown(2))
-        {
-            //Debug.Log("Pressed middle click.");
+            transform.Translate(new Vector3((MiddleClick.x - mousePosition.x) * Time.deltaTime, (MiddleClick.y - mousePosition.y) * Time.deltaTime, 0));
             MiddleClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-            IsMiddleClicked = true;
+            //transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
         }
-
-        if (Input.GetMouseButtonUp(2))
-        {
-            //Debug.Log("Released middle click.");
-            IsMiddleClicked = false;
-        }
-
-        if (IsMiddleClicked)
-            MoveMiddlePressed();
-        #endregion MoveCamera
-
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
-            MoveCamera(1);
-        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
-            MoveCamera(-1);
-    }
-
-    private void MoveCamera(int step)
-    {
-        if (step > 0)
-            transform.Translate(Vector3.forward);
-        else
-            transform.Translate(Vector3.back);
-        //var pos = transform.position;
-        //pos.z += step;
-        //transform.position = pos;
-    }
-
-    private void MoveRightPressed()
-    {
-        const float velocity = (float)0.001;
-
-        var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-        //var rot = transform.rotation;
-        ////Debug.Log(string.Format("Rot {0}, {1}, {2}", pos.x, pos.y, pos.z));
-        //rot.x += velocity * (RightClick.y - mousePosition.y);
-        ////pos.x += (float)0.01;
-        //rot.y -= velocity * (RightClick.x - mousePosition.x);
-        //transform.rotation = rot;
-        //RightClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-        //transform.RotateAround(mousePosition, Vector3.up, Input.GetAxis("Mouse X") * velocity);
-
-        //float tiltAroundZ = Input.GetAxis("Horizontal") * 15;
-        //float tiltAroundX = Input.GetAxis("Vertical") * 15;
-        //Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
-        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 15);
-
-        transform.RotateAround(transform.position, new Vector3(1.0f, 0.0f, 0.0f), (RightClick.y - mousePosition.y) * Time.deltaTime * 5);
-        transform.RotateAround(transform.position, new Vector3(0.0f, 1.0f, 0.0f), -(RightClick.x - mousePosition.x) * Time.deltaTime * 5);
-        transform.RotateAround(transform.position, new Vector3(0.0f, 0.0f, 1.0f), (RightClick.z - mousePosition.z) * Time.deltaTime * 5);
-        RightClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-    }
-
-    private void MoveMiddlePressed()
-    {
-        const float speed = (float)0.1;
-        var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-        //var pos = transform.position;
-        //pos.x += speed * (MiddleClick.x - mousePosition.x);
-        //pos.y += speed * (MiddleClick.y - mousePosition.y);
-        //transform.position = pos;
-
-        transform.Translate(new Vector3((MiddleClick.x - mousePosition.x) * Time.deltaTime, (MiddleClick.y - mousePosition.y) * Time.deltaTime, 0));
-        MiddleClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-        //transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
     }
 }
