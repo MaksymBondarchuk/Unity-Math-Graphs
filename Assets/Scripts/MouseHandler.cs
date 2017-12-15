dd4dfeaa2146b6f4e5f9b5febbf74b06de5221dc
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2017-12-15 19:28:25 +0200

    Updated MouseHandler

--- Assets/Scripts/MouseHandler.cs
+++ Assets/Scripts/MouseHandler.cs
@@ -1,81 +1,116 @@
-﻿using UnityEngine;
-// ReSharper disable UnusedMember.Local
+﻿// ReSharper disable All
+using UnityEngine;
+using System.Collections;
 
-namespace Assets.Scripts
+
+[AddComponentMenu("Camera-Control/3dsMax Camera Style")]
+public class MouseHandler : MonoBehaviour
 {
-    // ReSharper disable once UnusedMember.Global
-    public class MouseHandler : MonoBehaviour
+    public Transform target;
+    public Vector3 targetOffset;
+    public float distance = 5.0f;
+    public float maxDistance = 20;
+    public float minDistance = .6f;
+    public float xSpeed = 200.0f;
+    public float ySpeed = 200.0f;
+    public int yMinLimit = -80;
+    public int yMaxLimit = 80;
+    public int zoomRate = 40;
+    public float panSpeed = 0.3f;
+    public float zoomDampening = 5.0f;
+
+    private float xDeg = 0.0f;
+    private float yDeg = 0.0f;
+    private float currentDistance;
+    private float desiredDistance;
+    private Quaternion currentRotation;
+    private Quaternion desiredRotation;
+    private Quaternion rotation;
+    private Vector3 position;
+
+    void Start() { Init(); }
+    void OnEnable() { Init(); }
+
+    public void Init()
     {
-        private Vector3 RightClick { get; set; }
-
-        private bool IsRightClicked { get; set; }
-
-        private Vector3 MiddleClick { get; set; }
-
-        private bool IsMiddleClicked { get; set; }
-
-        private void Start()
+        //If there is no target, create a temporary target at 'distance' from the cameras current viewpoint
+        if (!target)
         {
-            Cursor.visible = true;
+            GameObject go = new GameObject("Cam Target");
+            go.transform.position = transform.position + (transform.forward * distance);
+            target = go.transform;
         }
 
-        private void Update()
+        distance = Vector3.Distance(transform.position, target.position);
+        currentDistance = distance;
+        desiredDistance = distance;
+
+        //be sure to grab the current rotations as starting points.
+        position = transform.position;
+        rotation = transform.rotation;
+        currentRotation = transform.rotation;
+        desiredRotation = transform.rotation;
+
+        xDeg = Vector3.Angle(Vector3.right, transform.right);
+        yDeg = Vector3.Angle(Vector3.up, transform.up);
+    }
+
+    /*
+     * Camera logic on LateUpdate to only update after all character movement logic has been handled. 
+     */
+    void LateUpdate()
+    {
+        // If Control and Alt and Middle button? ZOOM!
+        if (Input.GetMouseButton(2) && Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.LeftControl))
         {
-            #region MoveCamera
-            if (Input.GetMouseButtonDown(1))
-            {
-                RightClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-                IsRightClicked = true;
-            }
+            desiredDistance -= Input.GetAxis("Mouse Y") * Time.deltaTime * zoomRate * 0.125f * Mathf.Abs(desiredDistance);
+        }
+        // If middle mouse and left alt are selected? ORBIT
+        else if (Input.GetMouseButton(2) && Input.GetKey(KeyCode.LeftAlt))
+        {
+            xDeg += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+            yDeg -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
 
-            if (Input.GetMouseButtonUp(1))
-            {
-                IsRightClicked = false;
-            }
+            ////////OrbitAngle
 
-            if (IsRightClicked)
-                MoveRightPressed();
+            //Clamp the vertical axis for the orbit
+            yDeg = ClampAngle(yDeg, yMinLimit, yMaxLimit);
+            // set camera rotation 
+            desiredRotation = Quaternion.Euler(yDeg, xDeg, 0);
+            currentRotation = transform.rotation;
 
-            if (Input.GetMouseButtonDown(2))
-            {
-                MiddleClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-                IsMiddleClicked = true;
-            }
-
-            if (Input.GetMouseButtonUp(2))
-            {
-                IsMiddleClicked = false;
-            }
-
-            if (IsMiddleClicked)
-                MoveMiddlePressed();
-            #endregion MoveCamera
-
-            if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
-                MoveCamera(1);
-            else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
-                MoveCamera(-1);
+            rotation = Quaternion.Lerp(currentRotation, desiredRotation, Time.deltaTime * zoomDampening);
+            transform.rotation = rotation;
+        }
+        // otherwise if middle mouse is selected, we pan by way of transforming the target in screenspace
+        else if (Input.GetMouseButton(2))
+        {
+            //grab the rotation of the camera so we can move in a psuedo local XY space
+            target.rotation = transform.rotation;
+            target.Translate(Vector3.right * -Input.GetAxis("Mouse X") * panSpeed);
+            target.Translate(transform.up * -Input.GetAxis("Mouse Y") * panSpeed, Space.World);
         }
 
-        private void MoveCamera(int step)
-        {
-            transform.Translate(step > 0 ? Vector3.forward : Vector3.back);
-        }
+        ////////Orbit Position
 
-        private void MoveRightPressed()
-        {
-            var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-            transform.RotateAround(transform.position, new Vector3(1.0f, 0.0f, 0.0f), (RightClick.y - mousePosition.y) * Time.deltaTime * 5);
-            transform.RotateAround(transform.position, new Vector3(0.0f, 1.0f, 0.0f), -(RightClick.x - mousePosition.x) * Time.deltaTime * 5);
-            transform.RotateAround(transform.position, new Vector3(0.0f, 0.0f, 1.0f), (RightClick.z - mousePosition.z) * Time.deltaTime * 5);
-            RightClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-        }
+        // affect the desired Zoom distance if we roll the scrollwheel
+        desiredDistance -= Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * zoomRate * Mathf.Abs(desiredDistance);
+        //clamp the zoom min/max
+        desiredDistance = Mathf.Clamp(desiredDistance, minDistance, maxDistance);
+        // For smoothing of the zoom, lerp distance
+        currentDistance = Mathf.Lerp(currentDistance, desiredDistance, Time.deltaTime * zoomDampening);
 
-        private void MoveMiddlePressed()
-        {
-            var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-            transform.Translate(new Vector3((MiddleClick.x - mousePosition.x) * Time.deltaTime, (MiddleClick.y - mousePosition.y) * Time.deltaTime, 0));
-            MiddleClick = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-        }
+        // calculate position based on the new currentDistance 
+        position = target.position - (rotation * Vector3.forward * currentDistance + targetOffset);
+        transform.position = position;
+    }
+
+    private static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
     }
 }
